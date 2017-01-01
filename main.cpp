c904dbc38efdb02184ad816c308b3017b9e2c426
Alex Ander <loktionov129@gmail.com>
2017-01-02 00:19:46 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
-#include "game.h"
+#define CONSOLE_GAME 1
+
+#if CONSOLE_GAME == 1
+#include "game_con.h"
+#elif defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#include "game_win.h"
+#else
+#include "game_gtk.h"
+#endif
 
 int main(int argc, char *argv[])
 {
