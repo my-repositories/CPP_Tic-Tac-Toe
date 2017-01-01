9dcd5f939b5013c0519fc6b35881d214432df550
Alex Ander <loktionov129@gmail.com>
2017-01-02 00:44:46 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
-#define CONSOLE_GAME 1
+// CONSOLE (iostream + cstdlib)
+#define CONSOLE_GAME 0
 
-#if CONSOLE_GAME == 1
+// QT (Cross-platform application framework)
+#define QT_GAME 1
+
+// WIN (Windows.h || gtk.h)
+#define WIN_GAME 2
+
+// Choose Your Destiny
+#define MY_GAME CON_GAME
+
+#if MY_GAME == CONSOLE_GAME
 #include "game_con.h"
+#elif MY_GAME == QT_GAME
+#include "game_qt.h"
 #elif defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 #include "game_win.h"
 #else
