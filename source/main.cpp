420158588ba704f4bc2c102fc4a92ea255fa2325
Alex Ander <loktionov129@gmail.com>
2017-01-02 01:04:40 +0700

    Create main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,18 @@
+#include "settings.h"
+
+#if MY_GAME == CONSOLE_GAME
+#include "game_con.h"
+#elif MY_GAME == QT_GAME
+#include "game_qt.h"
+#elif defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#include "game_win.h"
+#else
+#include "game_gtk.h"
+#endif
+
+int main(int argc, char *argv[])
+{
+    Game game;
+    game.Start();
+    return 0;
+}
