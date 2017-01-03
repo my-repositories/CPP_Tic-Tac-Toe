45458811b936c4c3714383e17c81be0798387494
Alex Ander <loktionov129@gmail.com>
2017-01-03 12:10:22 +0700

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,15 @@
-#include "settings.h"
+#include "../include/settings.h"
 
 #if MY_GAME == CONSOLE_GAME
-#include "game_con.h"
+#include "../include/game_con.h"
 #elif MY_GAME == QT_GAME
-#include "game_qt.h"
+#include "../include/game_qt.h"
 #elif MY_GAME == SFML_GAME
-#include "game_sfml.h"
+#include "../include/game_sfml.h"
 #elif defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-#include "game_win.h"
+#include "../include/game_win.h"
 #else
-#include "game_gtk.h"
+#include "../include/game_gtk.h"
 #endif
 
 #if MY_GAME != QT_GAME
@@ -23,9 +23,7 @@ int main(int argc, char *argv[])
 
 #else
 
-#include "game_qt.h"
 #include <QApplication>
-
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
