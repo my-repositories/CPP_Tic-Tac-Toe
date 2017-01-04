6e137f926cfd4615ed1230718ec6f81527e6c95f
Alex Ander <loktionov129@gmail.com>
2017-01-04 22:54:48 +0700

    Update board.cpp

--- source/board.cpp
+++ source/board.cpp
@@ -33,7 +33,7 @@ int Board::GameOver() const
     // Calling each check function
     for (int i = 0; i < 4; i++)
     {
-        state = CheckWin(this, checks[i]);
+        state = CheckWin(checks[i]);
         if (state == STATE_X_WINNER || state == STATE_O_WINNER)
             return state;
     }
@@ -109,7 +109,7 @@ int Board::CheckDraw() const
 }
 
 // Check for winner ( condition: 'int m_win_streak' in a row )
-int Board::CheckWin(const Board *board, int(Board::*pCheck)(int) const) const
+int Board::CheckWin(int(Board::*pCheck)(int) const) const
 {
     int state;
 
@@ -117,7 +117,7 @@ int Board::CheckWin(const Board *board, int(Board::*pCheck)(int) const) const
     // then Checkwin second player on the field (player = 2)
     for (int player = 1; player < 3; player++)
     {
-        state = (board->*pCheck)(player);
+        state = (this->*pCheck)(player);
         if (state == STATE_X_WINNER || state == STATE_O_WINNER)
             return state;
     }
@@ -151,45 +151,6 @@ int Board::CheckAntiDiagonal(int player) const
 
     return STATE_NEXT_STEP;
 }
-
-// save backuyp
-#if 0
-// Check Secondary diagonal
-int Board::CheckAntiDiagonal(int player) const
-{
-    int streak_count = 0;
-    for (int i = 0; i < m_field_size; i++)
-    {
-        if (board[(m_field_size - i - 1)][i] == player)
-            ++streak_count;
-        else
-            streak_count = 0;
-
-        if (streak_count == m_win_streak)
-            return player;
-    }
-
-    return STATE_NEXT_STEP;
-}
-
-// Check Main diagonal
-int Board::CheckDiagonal(int player) const
-{
-    int streak_count = 0;
-    for (int i = 0; i < m_field_size; i++)
-    {
-        if (board[i][i] == player)
-            ++streak_count;
-        else
-            streak_count = 0;
-
-        if (streak_count == m_win_streak)
-            return player;
-    }
-
-    return STATE_NEXT_STEP;
-}
-#endif
 
 // Check Main diagonals
 int Board::CheckDiagonal(int player) const
@@ -258,3 +219,90 @@ int Board::CheckHorizontal(int player) const
 
     return STATE_NEXT_STEP;
 }
+
+// save backuyp
+#if 0
+
+// Check game state ( DRAW | X_WIN | O_WIN | NEXT_STEP )
+int Board::GameOver() const
+{
+    int state;
+
+    // Array of check functions
+    static int(Board::*checks[])(int) const =
+    {
+        &Board::CheckAntiDiagonal,
+        &Board::CheckDiagonal,
+        &Board::CheckVertical,
+        &Board::CheckHorizontal
+    };
+
+    // Calling each check function
+    for (int i = 0; i < 4; i++)
+    {
+        state = CheckWin(this, checks[i]);
+        if (state == STATE_X_WINNER || state == STATE_O_WINNER)
+            return state;
+    }
+
+    // check for tie
+    state = CheckDraw();
+    if (state == STATE_DRAW)
+        return STATE_DRAW;
+
+    return STATE_NEXT_STEP;
+}
+
+// Check for winner ( condition: 'int m_win_streak' in a row )
+int Board::CheckWin(const Board *board, int(Board::*pCheck)(int) const) const
+{
+    int state;
+
+    // Checkwin first player on the field (player = 1)
+    // then Checkwin second player on the field (player = 2)
+    for (int player = 1; player < 3; player++)
+    {
+        state = (board->*pCheck)(player);
+        if (state == STATE_X_WINNER || state == STATE_O_WINNER)
+            return state;
+    }
+
+    return STATE_NEXT_STEP;
+}
+
+// Check Secondary diagonal
+int Board::CheckAntiDiagonal(int player) const
+{
+    int streak_count = 0;
+    for (int i = 0; i < m_field_size; i++)
+    {
+        if (board[(m_field_size - i - 1)][i] == player)
+            ++streak_count;
+        else
+            streak_count = 0;
+
+        if (streak_count == m_win_streak)
+            return player;
+    }
+
+    return STATE_NEXT_STEP;
+}
+
+// Check Main diagonal
+int Board::CheckDiagonal(int player) const
+{
+    int streak_count = 0;
+    for (int i = 0; i < m_field_size; i++)
+    {
+        if (board[i][i] == player)
+            ++streak_count;
+        else
+            streak_count = 0;
+
+        if (streak_count == m_win_streak)
+            return player;
+    }
+
+    return STATE_NEXT_STEP;
+}
+#endif
