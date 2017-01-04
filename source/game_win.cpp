8af6615f276ca6a2910189f17ede94b78b33fe60
Alex Ander <loktionov129@gmail.com>
2017-01-04 11:37:22 +0700

    Delete game_win.cpp

--- source/game_win.cpp
+++
@@ -1,6 +0,0 @@
-#include "settings.h"
-#if MY_GAME == WIN_GAME \
-    && !defined(__CYGWIN__) \
-    && (defined(WIN32) || defined(_WIN32) || defined(__WIN32))
-#include "game_win.h"
-#endif // !WIN_GAME
